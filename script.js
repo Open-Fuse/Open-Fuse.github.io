31e17528253c3fc64c76021cba2e1e46a9e90b34
Demo User <demouser@MacBook-Air.local>
2017-07-12 17:04:24 -0400

    forms

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+function postComment() {
+  var tempComment = $("#comment").val();
+  var tempName = $("#name").val();
+  $("body").after("<br><br>" + tempName + " | " + tempComment);
+}
+
+function setup()
+{
+  $("#send_button").click(postComment);
+}
+
+$(document).ready(setup)
